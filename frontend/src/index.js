0343cb64704a4d14dc7a152bb043f4119c02ad43
Guilherme <gui.miguel99@gmail.com>
2020-04-29 15:46:51 -0300

    fix index

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <BrowserRouter>
+        <Route path="/" component={ App }/>
+    </BrowserRouter>, document.getElementById('root')
 );
